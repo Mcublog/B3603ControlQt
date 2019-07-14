d39dd6d794c2c567699d14e3c19fabfd8b6affc5
Viacheslav <viacheslav@mcublog.xyz>
2019-07-14 13:58:33 +0300

    added config.json added voltage offset

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 ﻿#!/usr/bin/python -u
 import sys, os
 import serial, time
+import json
 
 sys.path.insert(0, os.getcwd() + '\\ui')
 sys.path.insert(0, os.getcwd() + '\\control')
@@ -20,6 +21,18 @@ class ExampleApp(QtWidgets.QMainWindow, main_ui.Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Init design
+
+        try:
+            offset = 0
+            with open("config.json", "r") as config_file:
+                config = json.load(config_file)
+            print("Config found...")
+            self.leVoltage.setText(str(config['voltage']))
+            self.leVoltageOfst.setText(str(config['voltage offset']))
+        except:
+            print("Config not found")
+            update_config(0)
+
         # Slot connecting
         self.pbOff.clicked.connect(self.set_off)
         self.pbSet.clicked.connect(self.set_on)
@@ -34,16 +47,30 @@ class ExampleApp(QtWidgets.QMainWindow, main_ui.Ui_MainWindow):
 
     def set_on(self):
         v = self.leVoltage.text()
+        ofst = self.leVoltageOfst.text()
+        self.update_config(int(v), int(ofst))
+        volt = int(v) - int(ofst)
 
         cmdr = Control('COM9')  # /dev/ttyUSB0 for Linux
         if cmdr.get_status() == 0:
             return close_connect(cmdr)
 
-        print('Voltage: ' + v)
-        cmdr.send_cmd("VOLTAGE " + v)
-        cmdr.send_cmd("CURRENT 400")
+        print('Voltage: ' + str(volt))
+        cmdr.send_cmd("VOLTAGE " + str(volt))
+        cmdr.send_cmd("CURRENT 1000")
         cmdr.send_cmd("OUTPUT 1")
         close_connect(cmdr)
+
+    def update_config(self, voltage, ofst):
+        config = {
+            "voltage": voltage,
+            "voltage offset": ofst
+        }
+        cfg = json.dumps(config, indent=4, sort_keys=True)
+        print(cfg)
+        f = open("config.json", 'w')
+        f.write(cfg)
+        f.close()
 
 
 
